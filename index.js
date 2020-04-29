0ea776df62185e8b1f0d6c2dd193f853efc70851
msteele11101 <msteele11101@gmail.com>
2020-04-28 18:16:01 -0700

    MVP

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//set up server object
+const server = require('./api/server.js');
+
+//set up ports
+const PORT = process.env.PORT || 4001;
+
+//listen to the server
+server.listen(PORT, () => console.log(`\n===Running on localhost:${PORT}===\n`))
